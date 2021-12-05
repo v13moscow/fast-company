8061c04662fc6568dbda1ccb0d71621f995f380a
Vadim Moroz <v9858480345@yandex.ru>
2021-12-05 12:43:12 +0300

    creating a user table

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,8 +1,89 @@
-import React from "react"
+import React,{useState} from "react"
 import api from "../api/"
 
 const Users = () => {
-  console.log(api.users.fetchAll());
-  return <h1>Users</h1>
+  const [users, setUsers] = useState(api.users.fetchAll())
+  
+  const renderFrase = (number) => {
+    let numeral = ''
+    number === 2 || number === 3 || number === 4 ? numeral = 'человека' : numeral = 'человек'
+    return <h2><span className="badge bg-primary bg-lg p-2">{number} {numeral} тусанет с тобой сегодня</span></h2>
+  }
+  const handleDelete = (id) => {
+    setUsers(prevState=>prevState.filter(item =>item._id !== id))
+  }
+
+  const renderUsers = () => {
+    let num = users.length  
+    if(users.length === 0) return <h2><span className="badge bg-danger bg-lg p-2">Никто с тобой не тусанет</span></h2>  
+    return users.length !== 0 
+    && <div>
+        {renderFrase(num)}
+        <table className="table"> 
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user =>(   
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>
+                  {user.qualities.map(qual =>{
+                  let classes ="badge m-1 bg-"
+                  if(qual.color === "primary"){
+                    return <span key={qual._id}
+                    className={classes+='primary'}
+                    >{qual.name}</span>
+                  }if(qual.color === "secondary"){
+                    return <span key={qual._id}
+                    className={classes+='secondary'}
+                    >{qual.name}</span>
+                  }if(qual.color === "success"){
+                    return <span key={qual._id}
+                    className={classes+='success'}
+                    >{qual.name}</span>
+                  }if(qual.color === "danger"){
+                    return <span key={qual._id}
+                    className={classes+='danger'}
+                    >{qual.name}</span>
+                  }if(qual.color === "info"){
+                    return <span key={qual._id}
+                    className={classes+='info'}
+                    >{qual.name}</span>
+                  }if(qual.color === "dark"){
+                    return <span key={qual._id}
+                    className={classes+='dark'}
+                    >{qual.name}</span>
+                  }
+                    console.log(qual.color);
+                  })}
+                </td>
+                <td>{user.profession.name}</td>
+                <td>{user.completedMeetings}</td>
+                <td>{user.rate}/5</td>
+                <td><button
+                      className="btn btn-danger"
+                      onClick={() => handleDelete(user._id)}
+                      >delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+  }
+
+  return(
+    <div>
+      {renderUsers()}
+    </div>
+  ) 
 }
 export default Users;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import Users from './components/users'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
   <React.StrictMode>
